44b100e520347140757d1cc396fe769e1df71a46
davidhbtlr <54726058+davidhbtlr@users.noreply.github.com>
2020-07-06 21:52:25 -0400

    Futzing with animations
    Fixed some formatting and added back in some animations
    
    CRUCIAL ISSUE: You cannot scroll back to the start (need to set the animations on the first panel to never replay)

--- hey-you-v2/script.js
+++ hey-you-v2/script.js
@@ -1,138 +1,133 @@
 
 ScrollTrigger.defaults({
- toggleActions: "restart pause reverse pause",
-});
+  toggleActions: "restart pause reverse pause",
+ });
+ 
+ // PROGRESS HEADER
+ gsap.from(".header", {
+   scrollTrigger: ".sms1",
+   trigger: ".sms1",
+   y:-60,
+   opacity: 0,
+   duration: 3,});
+ 
+ const tl = gsap.timeline({
+ scrollTrigger: {
+   id: "ProgressBar",
+   trigger: "top",
+   endTrigger: ".sms11",
+   end: "top",
+   scrub: 1,
+   markers: false,}  
+ });
+ 
+ tl.to(".progress-bar", {
+   width: "105%",
+ })
+ 
 
-// PROGRESS HEADER
-gsap.from(".header", {
-  scrollTrigger: ".sms2",
-  trigger: ".sms2",
-  y:-60,
-  opacity: 0,
-  duration: 1,});
+ gsap.to(".scroll-down", {
+   y:-20, 
+   ease:Bounce,
+ delay: 2,
+   duration: 1,
+   repeat: -1,
+   yoyo: 1,
+ });
+ 
+ 
+ // gsap.to(".logo div", {y:10, ease:Bounce.easeOut});
+ 
+ // PANEL ONE 
+ 
+  gsap.from(".sms1 div", {
 
-const tl = gsap.timeline({
-scrollTrigger: {
-  id: "ProgressBar",
-  trigger: ".sms3",
-  endTrigger: ".register",
-  end: "top",
-  scrub: 1,
-  markers: false,}  
-});
+    scrollTrigger: ".sms1", 
+   duration: 1,
+   scale: 1.2,
+   ease:Elastic.easeOut});
+ 
+  gsap.from(".sms2 div", {
 
-tl.to(".progress-bar", {
-  width: "100%",
-})
+    scrollTrigger: ".sms2", 
+   duration: 1.25,
+   scale: 1.2,
+  ease:Elastic.easeOut});
+ 
+ 
+  gsap.from(".sms3 div", {
 
-// LOGO PANEL
-gsap.from(".logo-container div", {
-  scale:.5, 
-  ease:Bounce.easeOut,
-delay: .7,
-});
-gsap.to(".logo-container div", {
-  y:-20, 
-  ease:Bounce,
-delay: 2,
-  duration: 1,
-  repeat: -1,
-  yoyo: 1,
-});
+    scrollTrigger: ".sms3", 
+   duration: 1.75,
+   scale: 1.2,
+  ease:Elastic.easeOut});
+ 
+  gsap.from(".sms4 div", {
 
+    scrollTrigger: ".sms4", 
+   duration: 2,
+   scale: 1.2,
+  ease:Elastic.easeOut});
+ 
 
-// gsap.to(".logo div", {y:10, ease:Bounce.easeOut});
-
-// PANEL ONE 
-
- gsap.to(".sms1 div", {
-  scrollTrigger: ".sms1", 
-  repeat: 1,
-  duration: .75,
-     delay: .25,
-  y:5,
- ease:Bounce.easeOut});
-
- gsap.to(".sms2 div", {
-  scrollTrigger: ".sms2", 
-  delay: .5,
-   repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
-// PANEL TWO 
-
- gsap.to(".sms3 div", {
-  scrollTrigger: ".sms3", 
-  repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
- gsap.to(".sms4 div", {
-  scrollTrigger: ".sms4", 
-  delay: .25,
-   repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
-gsap.to(".sms5 div", {
-  scrollTrigger: ".sms5", 
-  delay: .5,
-   repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
-// PANEL THREE 
-
- gsap.to(".sms6 div", {
+ // PANEL TWO  
+ gsap.to(".logo-container div", {
   scrollTrigger: ".sms6", 
   repeat: 1,
   duration: .75,
   y:5,
  ease:Bounce.easeOut});
 
-// PANEL FOUR 
+ // PANEL THREE 
 
- gsap.to(".sms7 div", {
-  scrollTrigger: ".sms7", 
-  repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
+ gsap.to(".sms5 div", {
+   scrollTrigger: ".sms5", 
+   delay: .5,
+    repeat: 1,
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
 
- gsap.to(".sms8 div", {
-  scrollTrigger: ".sms8", 
-  delay: .25,
+  gsap.to(".sms7 div", {
+   scrollTrigger: ".sms7", 
    repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
+ 
+  gsap.to(".sms8 div", {
+   scrollTrigger: ".sms8", 
+   delay: .25,
+    repeat: 1,
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
+ 
+ // PANEL FOUR 
+ //  Animation for registration section goes here
 
-// PANEL FIVE 
-
- gsap.to(".sms9 div", {
-  scrollTrigger: ".sms9", 
-  repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
- gsap.to(".sms10 div", {
-  scrollTrigger: ".sms10", 
-  delay: .25,
+ // PANEL FIVE
+ 
+  gsap.to(".sms9 div", {
+   scrollTrigger: ".sms9", 
    repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
-
-gsap.to(".sms11 div", {
-  scrollTrigger: ".sms11", 
-  delay: .5,
-   repeat: 1,
-  duration: .75,
-  y:5,
- ease:Bounce.easeOut});
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
+ 
+  gsap.to(".sms10 div", {
+   scrollTrigger: ".sms10", 
+   delay: .25,
+    repeat: 1,
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
+ 
+ gsap.to(".sms11 div", {
+   scrollTrigger: ".sms11", 
+   delay: .5,
+    repeat: 1,
+   duration: .75,
+   y:5,
+  ease:Bounce.easeOut});
+ 
